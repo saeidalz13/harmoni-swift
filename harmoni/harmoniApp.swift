576b43766a7b91de547170ab3163d1a7d6ddfd52
Saeid Alizadeh <saeidalz96@gmail.com>
2025-01-25 06:53:22 -0500

    Initial Commit

---
+++ harmoni/harmoniApp.swift
@@ -0,0 +1,17 @@
+//
+//  harmoniApp.swift
+//  harmoni
+//
+//  Created by Saeid Alizadeh on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct harmoniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ harmoniTests/harmoniTests.swift
@@ -0,0 +1,17 @@
+//
+//  harmoniTests.swift
+//  harmoniTests
+//
+//  Created by Saeid Alizadeh on 2025-01-25.
+//
+
+import Testing
+@testable import harmoni
+
+struct harmoniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
